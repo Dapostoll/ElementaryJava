5d8fcd520952ef9bc2a263f8cdf22f33341481d8
Dapostoll <1Dantver1@gmail.com>
2021-05-27 02:04:38 +0300

    lesson01

--- src/homeWork/lesson1/Main.java
+++ src/homeWork/lesson1/Main.java
@@ -5,32 +5,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Animal objectsphinx = new Animal("Sphinx");
-
-
         Map<String, String> catLinkedHashMap = new LinkedHashMap<>();
         ArrayList dogArrayList = new ArrayList();
         LinkedList catLinkedList = new LinkedList();
         Map hastable = new Hashtable();
         Map treeMap = new TreeMap();
         List stack = new Stack();
-
         Set iset = new HashSet();
-
         iset.add(objectsphinx);
         Iterator itr = iset.iterator();
         while (itr.hasNext()) {
             System.out.println(itr.next().toString());
         }
-
-
         stack.add(objectsphinx);
         stack.add("1234");
         stack.add(objectsphinx);
         System.out.println(stack);
         stack.remove(1);
         System.out.println(stack);
-
-
         System.out.println(hastable);
         hastable.put(1, 2);
         treeMap.putAll(hastable);
@@ -40,14 +32,10 @@ public class Main {
         hastable.equals(objectsphinx);
         hastable.remove(1);
         hastable.put(objectsphinx, 23);
-
-
         System.out.println(hastable);
         catLinkedHashMap.put("Sphinx", "Sphinx");
         System.out.println(catLinkedHashMap);
         catLinkedHashMap.clear();
-
-
         System.out.println(catLinkedHashMap);
         String sphinx = "Sphinx ";
         String siamese = "Siamese ";
@@ -69,14 +57,11 @@ public class Main {
         System.out.println(dogArrayList);
         dogArrayList.removeAll(dogArrayList);
         System.out.println(dogArrayList);
-
         String scottishLopEared = "Scottish Lop-eared";
         System.out.println(catLinkedList);
         catLinkedList.add(scottishLopEared);
         catLinkedList.add(englishBulldog);
         catLinkedList.add(beagle);
         System.out.println(catLinkedList);
-//
-
     }
 }
